26901a477f6c024aad4f54ab65ec83b2bd4e9152
root <root@localhost.localdomain>
2020-11-08 16:06:20 +0000

    Updated a test to use a parametrised input

--- tests/test_wps_extract_uk_station_data.py
+++ tests/test_wps_extract_uk_station_data.py
@@ -6,6 +6,8 @@ from pywps.tests import assert_response_success
 from .common import get_output, run_with_inputs, check_for_output_file
 from goldfinch.processes.wps_extract_uk_station_data import ExtractUKStationData
 
+data_inputs = ['obs_table=TD;counties=devon;start=2017-10-01T00:00:00;end=2018-01-31T00:00:00',
+               'obs_table=TD;counties=surrey;start=2017-10-01T00:00:00;end=2018-01-31T00:00:00']
 
 def test_wps_extract_uk_station_data_no_params_fail():
     datainputs = ""
@@ -22,9 +24,9 @@ def test_wps_extract_uk_station_data_no_table_fail():
     assert "please provide: ['obs_table']" in resp.response[0].decode('utf-8')
 
 
-def test_wps_extract_uk_station_data_one_county_success():
-    datainputs = "obs_table=RD;counties=devon;start=2017-10-01T00:00:00;end=2018-01-31T00:00:00"
-    resp = run_with_inputs(ExtractUKStationData, datainputs)
+@pytest.mark.parametrize('ex_input', data_inputs)
+def test_wps_extract_uk_station_data_one_county_success(ex_input):
+    resp = run_with_inputs(ExtractUKStationData, ex_input)
 
     assert_response_success(resp)
     output = get_output(resp.xml)
