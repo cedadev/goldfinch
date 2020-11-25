63f914833ed24dbcb378ccfec6e150e534e17308
Jonathan Haigh <jhaigh0@sci2.jasmin.ac.uk>
2020-11-25 17:07:07 +0000

    Added a test declaratuion with pytest parametrize

--- tests/test_wps_compare_get_weather_stations.py
+++ tests/test_wps_compare_get_weather_stations.py
@@ -1,7 +1,22 @@
 import pytest
+import itertools
 
 from pywps.tests import assert_response_success
 
 from .common import run_with_inputs, check_for_output_file
 from goldfinch.processes.wps_get_weather_stations import GetWeatherStations
 
+times = ['DateRange=2010-01-01/2010-12-31;', 'DateRange=2014-01-01/2014-12-31;',
+         'DateRange=2018-01-01/2018-12-31;']
+counties = ['counties=OXFORDSHIRE;','counties=DEVON;','counties=BERKSHIRE;counties=DORSET;',
+            'counties=YORKSHIRE;counties=SURREY;counties=HAMPSHIRE;']
+
+file_names = [name[0] + name[1] for name in itertools.product(['1','2','3'],['a','b','c','d'])]
+inputs = [inp[0] + inp[1] for inp in itertools.product(times, counties)]
+
+params = list(zip(file_names, inputs))
+
+@pytest.mark.parametrize("file_name,param", params)
+def test_compare_weather_station_output(file_name, param):
+
+    
