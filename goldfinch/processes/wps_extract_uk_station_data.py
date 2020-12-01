225fa33ab82a48f53864bc02ce0e936a89b4aef1
Jonathan Haigh <jhaigh0@sci2.jasmin.ac.uk>
2020-12-01 16:28:24 +0000

    Change block comment to single line comments

--- goldfinch/processes/wps_extract_uk_station_data.py
+++ goldfinch/processes/wps_extract_uk_station_data.py
@@ -242,12 +242,12 @@ class ExtractUKStationData(Process):
 
         n_years =  int(inputs['end'][:4]) - int(inputs['start'][:4])
 
-"""         if n_stations > STATION_LIMIT and inputs['chunk_rule'] == 'decadal':
-
-            inputs['chunk_rule'] = 'year'
-            raise Exception('The number of selected station IDs has been calculated to be '
-                            'greater than {}. Please select a chunk size other than "decadal" '
-                            'for such a large volume of data.'.format(STATION_LIMIT)) """
+#        if n_stations > STATION_LIMIT and inputs['chunk_rule'] == 'decadal':
+#
+#            inputs['chunk_rule'] = 'year'
+#            raise Exception('The number of selected station IDs has been calculated to be '
+#                            'greater than {}. Please select a chunk size other than "decadal" '
+#                            'for such a large volume of data.'.format(STATION_LIMIT))
 
         if n_years > 1 and n_stations > STATION_LIMIT:
             raise Exception('The number of selected station IDs has been calculated to be '
